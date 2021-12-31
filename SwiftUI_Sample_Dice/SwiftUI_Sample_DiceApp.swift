d400302dd212fc180f9a0bd6880ee92eccea03ee
katafuchix <katafuchix@gmail.com>
2022-01-01 02:42:52 +0900

    Initial Commit

---
+++ SwiftUI_Sample_Dice/SwiftUI_Sample_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Sample_DiceApp.swift
+//  SwiftUI_Sample_Dice
+//
+//  Created by cano on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Sample_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
